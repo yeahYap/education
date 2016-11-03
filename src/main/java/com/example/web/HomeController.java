ed3424d9ca78e6c60c099e3c1563875900bc45a9
HP\Mili <alfl930729@gmail.com>
2016-11-03 14:12:14 +0900

    질문 목록 기능 구현

--- src/main/java/com/example/web/HomeController.java
+++ src/main/java/com/example/web/HomeController.java
@@ -1,13 +1,21 @@
 package com.example.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	@GetMapping("")
-	public String home(){
+	public String home(Model model){
+		model.addAttribute("questions", questionRepository.findAll());
 		return "index";
 	}
 	
